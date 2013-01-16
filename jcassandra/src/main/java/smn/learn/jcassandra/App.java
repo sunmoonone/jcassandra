e5fd59419afbe580f45ccf8330210e084d714c43
sunmoonone <realsunmoonone@qq.com>
2013-01-16 10:12:19 +0800

    recreate jcassandra

---
+++ jcassandra/src/main/java/smn/learn/jcassandra/App.java
@@ -0,0 +1,13 @@
+package smn.learn.jcassandra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
